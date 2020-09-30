c72f2c75c60d64d9ba5e2fc99ce2695c43327426
dbendu <MAYagudin@sberbank.ru>
2020-09-30 18:32:11 +0300

    (коммит calesia) done parser without validation

--- srcs/printf_formatted.c
+++ srcs/printf_formatted.c
@@ -1,10 +1,86 @@
 #include "print_formatted.h"
 #include "ft_printf.h"
 #include <unistd.h>
-#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-bool	check_size(const char c);
-bool	check_type(const char c);
+bool	check_size(const char c)
+{
+	int		i;
+	char	sizes[7] = {'l', 'h', 'j', 'z', 't', 'L', '\0'};
+
+	i = 0;
+	while (sizes[i] != '\0')
+	{
+		if (c == sizes[i])
+			return (true);
+		++i;
+	}
+	return (false);
+}
+
+bool	check_type(const char c)
+{
+	int		i;
+	char	types[20] = {'d', 'i', 'o', 'u', 'x', 'X', 'f', 'F', 'e', 'E',
+						 'g', 'G', 'a', 'A', 'c', 's', 'S', 'p', 'n', '\0'};
+
+	i = 0;
+	while (types[i] != '\0')
+	{
+		if (c == types[i])
+			return (true);
+		++i;
+	}
+	return (false);
+}
+
+void	insert_type(const char **format, t_specifier *spec)
+{
+	int		i;
+	char	types[20] = {'d', 'i', 'o', 'u', 'x', 'X', 'f', 'F', 'e', 'E',
+						 'g', 'G', 'a', 'A', 'c', 's', 'S', 'p', 'n', '\0'};
+
+	i = 0;
+	while (types[i] != '\0')
+	{
+		if (**format == types[i])
+		{
+			spec->type = types[i];
+			++*format;
+			break;
+		}
+		++i;
+	}
+}
+
+void	insert_size(const char **format, t_specifier *spec)
+{
+	if (**format == 'l')
+		spec->size = 'l';
+	else if (**format == 'h')
+		spec->size = 'h';
+	else if (**format == 'j')
+		spec->size = 'j';
+	else if (**format == 'z')
+		spec->size = 'z';
+	else if (**format == 't')
+		spec->size = 't';
+	else if (**format == 'L')
+		spec->size = 'L';
+	if (spec->size != 0)
+	{
+		++*format;
+		if (!(check_type(**format)) && **format != '\0' && check_size(**format))
+		{
+			if (**format == 'l')
+				spec->double_l = true;
+			else
+				spec->double_h = true;
+			++*format;
+		}
+	}
+}
 
 void	insert_precision(const char **format, t_specifier *spec, va_list args)
 {
@@ -14,7 +90,7 @@ void	insert_precision(const char **format, t_specifier *spec, va_list args)
 	num[PRINTF_BUFSIZE - 1] = '\0';
 	if (**format == '*')
 	{
-		spec->width = va_arg(args, int);
+		spec->precision = va_arg(args, int);
 		++*format;
 	}
 	else if (**format != '\0' && **format == '.')
@@ -22,14 +98,14 @@ void	insert_precision(const char **format, t_specifier *spec, va_list args)
 		++*format;
 		if (**format == '*')
 		{
-			spec->width = va_arg(args, int);
+			spec->precision = va_arg(args, int);
 			++*format;
 		}
 		else
 		{
 			i = 0;
 			while (**format != '\0' && !(check_type(**format)) &&
-					!(check_size(**format)))
+				   !(check_size(**format)))
 			{
 				num[i] = **format;
 				++i;
@@ -37,7 +113,7 @@ void	insert_precision(const char **format, t_specifier *spec, va_list args)
 			}
 			num[i] = '\0';
 			if (i != 0)
-				spec->width = ft_atoi(num);
+				spec->precision = ft_atoi(num);
 		}
 	}
 }
@@ -55,11 +131,11 @@ bool	insert_width(const char **format, t_specifier *spec, va_list args)
 		return (true);
 	}
 	else if (**format != '\0' && **format != '*' && **format != '.' &&
-				!(check_type(**format)) && !(check_size(**format)))
+			 !(check_type(**format)) && !(check_size(**format)))
 	{
 		i = 0;
 		while (**format != '\0' && **format != '*' && **format != '.' &&
-				!(check_type(**format)) && !(check_size(**format)))
+			   !(check_type(**format)) && !(check_size(**format)))
 		{
 			num[i] = **format;
 			++i;
@@ -68,21 +144,6 @@ bool	insert_width(const char **format, t_specifier *spec, va_list args)
 		num[i] = '\0';
 		spec->width = ft_atoi(num);
 		return (true);
-	}
-	return (false);
-}
-
-bool	check_size(const char c)
-{
-	int		i;
-	char	sizes[7] = {'l', 'h', 'j', 'z', 't', 'L', '\0'};
-
-	i = 0;
-	while (sizes[i] != '\0')
-	{
-		if (c == sizes[i])
-			return (true);
-		++i;
 	}
 	return (false);
 }
@@ -103,34 +164,18 @@ void	insert_flags(t_specifier *spec, const char **format)
 		++*format;
 }
 
-bool	check_type(const char c)
+t_specifier	init_specifier(void)
 {
-	int		i;
-	char	types[20] = {'d', 'i', 'o', 'u', 'x', 'X', 'f', 'F', 'e', 'E',
-					'g', 'G', 'a', 'A', 'c', 's', 'S', 'p', 'n', '\0'};
+	t_specifier		spec;
 
-	i = 0;
-	while (types[i] != '\0')
-	{
-		if (c == types[i])
-			return (true);
-		++i;
-	}
-	return (false);
-}
-
-bool	init_specifier(t_specifier	*spec)
-{
-	if (!(spec = (t_specifier *)malloc(sizeof(t_specifier))))
-		return (false);
-	spec->flag = 0;
-	spec->width = 0;
-	spec->precision = 0;
-	spec->size = 0;
-	spec->double_h = false;
-	spec->double_l = false;
-	spec->type = 0;
-	return (true);
+	spec.flag = 0;
+	spec.width = 0;
+	spec.precision = 0;
+	spec.size = 0;
+	spec.double_h = false;
+	spec.double_l = false;
+	spec.type = 0;
+	return (spec);
 }
 
 /*
@@ -139,17 +184,25 @@ bool	init_specifier(t_specifier	*spec)
 
 int		main_handle(const char **format, va_list args)
 {
-	t_specifier		*spec;
+	t_specifier		spec;
 	int				res;
-	char			c;
+	// char			c;
 
 	res = 0;
-	if (!(init_specifier(spec)))
-		return (-1);
-	insert_flags(spec, format);
-	if (**format == '.' || (insert_width(format, spec, args)))
-		insert_precision(format,spec, args);
+	spec = init_specifier();
+	insert_flags(&spec, format);
+	if (**format == '.' || (insert_width(format, &spec, args)))
+		insert_precision(format,&spec, args);
+	insert_size(format, &spec);
+	insert_type(format, &spec);
 
+	printf("flag = %c\n", spec.flag);
+	printf("width = %lu\n", spec.width);
+	printf("precision = %lu\n", spec.precision);
+	printf("size = %c\n", spec.size);
+	printf("double_h = %d\n", spec.double_h);
+	printf("double_l = %d\n", spec.double_l);
+	printf("type = %c\n", spec.type);
 
 	// while (!(**format >= 'a' && **format <= 'z') && !(**format >= 'A' && **format <= 'Z'))
 	// {
